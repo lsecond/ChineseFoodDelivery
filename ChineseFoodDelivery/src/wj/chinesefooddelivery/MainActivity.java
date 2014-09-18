cbc430b74e06cabf82c0315ff4a267f861416c8e
weidaw <ouitatuwang@hotmail.com>
2014-09-17 20:49:48 -0400

    8:48

--- ChineseFoodDelivery/src/wj/chinesefooddelivery/MainActivity.java
+++ ChineseFoodDelivery/src/wj/chinesefooddelivery/MainActivity.java
@@ -11,14 +11,14 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+	} 
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
+	} // weida first commit 8:47
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
